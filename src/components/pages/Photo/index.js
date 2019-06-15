384f9f0927e18f0e21f987aba4e41fee7070294f
alex0735070005 <alex0735070005@gmail.com>
2019-06-15 15:46:20 +0300

    ddd

--- src/components/pages/Photo/index.js
+++ src/components/pages/Photo/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getPhoto } from '../../../utils/PhotoApiService';
 import './style.scss';
 
-function App() {
 
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+function Photo(props) {
+
+  const { match:{params:{photoId}}, setPhoto } = props;
+  
+  useEffect(()=> {
+
+    getPhoto(photoId).then(photo => {
+      setPhoto(photo)
+    });
+
+  }, [photoId, setPhoto]);
+
+  return <h1>Photo page</h1>
 }
 
-export default App;
+
+/*function Photo(props) {
+  
+  const { urls:{ thumb }, updated_at, alt_description, likes } = props.data;
+
+  const renderDate = () => {
+    const date = new Date(updated_at);
+    return date.toLocaleDateString('ru-RU');
+  }
+
+  return (
+    <div className="photo">
+        <span>{likes}</span>
+        <i>{renderDate()}</i>
+        <img src={thumb} alt={alt_description} />
+        <p>{alt_description}</p>
+    </div>
+  );
+}*/
+
+const mapStateToProps = state => ({
+  photo: state.photo.data
+})
+
+const mapDispatchToProps = dispatch => ({
+  setPhoto: photo => dispatch({type: 'SET_PHOTO', photo})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Photo);
